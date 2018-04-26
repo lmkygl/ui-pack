7aa7fc008724b44f18f1b7f6b3ebea4618c5f163
geunyoung <limgeunyoung90@gmail.com>
2018-04-26 10:59:20 +0900

    bxslide 플러그인 사요

--- src/js/client.js
+++ src/js/client.js
@@ -1,2 +1,2 @@
-console.log("module2"),$('[data-ui="navi_underline"]').on("mouseenter mouseleave",function(e){var n=$(e.target),o=e.type,t=n.find(".underline");"mouseenter"===o?TweenMax.to(t,.3,{width:"100%"}):"mouseleave"===o&&TweenMax.to(t,.3,{width:0})});
+$(".bxslider").bxSlider({mode:"fade"}),$('[data-ui="navi_underline"]').on("mouseenter mouseleave",function(e){var n=$(e.target),t=e.type,a=n.find(".underline");"mouseenter"===t?TweenMax.to(a,.3,{width:"100%"}):"mouseleave"===t&&TweenMax.to(a,.3,{width:0})});
 //# sourceMappingURL=client.js.map
---
+++ src/js/script/ui-bxslider.js
@@ -0,0 +1,6 @@
+(function(){
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        //captions: true,
+      });
+}());
