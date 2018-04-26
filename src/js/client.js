1e802ad40736b256d0a79a28727392f174822537
geunyoung <limgeunyoung90@gmail.com>
2018-04-26 17:00:38 +0900

    bxslide 기능

--- src/js/client.js
+++ src/js/client.js
@@ -1,2 +1,2 @@
-$(".bxslider").bxSlider({mode:"fade"}),$('[data-ui="navi_underline"]').on("mouseenter mouseleave",function(e){var n=$(e.target),t=e.type,a=n.find(".underline");"mouseenter"===t?TweenMax.to(a,.3,{width:"100%"}):"mouseleave"===t&&TweenMax.to(a,.3,{width:0})});
+$(".bxslider").bxSlider({mode:"horizontal"}),$('[data-ui="navi_underline"]').on("mouseenter mouseleave",function(e){var n=$(e.target),t=e.type,i=n.find(".underline");"mouseenter"===t?TweenMax.to(i,.3,{width:"100%"}):"mouseleave"===t&&TweenMax.to(i,.3,{width:0})});
 //# sourceMappingURL=client.js.map
--- src/js/script/ui-bxslider.js
+++ src/js/script/ui-bxslider.js
@@ -1,5 +1,10 @@
 (function () {
     $('.bxslider').bxSlider({
-        mode: 'fade'
+        mode: 'horizontal'
+        // Options
+        /*
+            mode : fade, vertical, horizontal
+            speed : 500 (숫자로 적용)
+        */
     });
 }());
