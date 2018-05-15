239f46e2ec53f0483af2e1e823789b65938da15f
geunyoung <limgeunyoung90@gmail.com>
2018-05-15 17:33:29 +0900

    tab 제작

--- src/js/script/ui-tab.js
+++ src/js/script/ui-tab.js
@@ -1,15 +1,16 @@
 // tab 제작
 (function () {
     var $tab_btn_wrap = $('[data-ui="tab_button_wrap"]');
-    var $this_tab_btn = $tab_btn_wrap.find('button');
-    var $pannel = $('.pannel').find('.pannel__list');
+    var $this_tab_btn = $tab_btn_wrap.find('.icon__sort__wrap');
+    var $pannel = $('.pannel__list__wrap').find('.pannel__list');
 
     
 
     function tab_button(e) {
         var $target = $(e.currentTarget);
         var tab_id = $target.attr('data-id');
-        var $tab_con = $('.pannel').find('[data-id="' + tab_id + '"]');
+        var $tab_con = $('.pannel__list__wrap').find('[data-id="' + tab_id + '"]');
+
         $this_tab_btn.removeClass('active');
         $target.addClass('active');
         $pannel.hide();
