444cca1a54b178a875c6bb37c551a20a42ec55ae
geunyoung <limgeunyoung90@gmail.com>
2018-05-18 13:59:33 +0900

    ui-tab과 fade 효과 합침

--- src/js/script/ui-tab-fade.js
+++ src/js/script/ui-tab-fade.js
@@ -1,38 +1,23 @@
-//tab 버튼 클릭시 fade in out
-(function () {
-    var $data = $('[data-id-value]');
-    var $tab_wrap = $('[data-ui="tab__btn__area"]');
-    var $tab_btn = $tab_wrap.find('.tab__btn');
-    var speed = 200;
+// //tab 버튼 클릭시 fade in out
+// (function () {
+//     var $data = $('[data-id]');
+//     var $tab_wrap = $('[data-ui="tab__btn__area"]');
+//     var $tab_btn = $tab_wrap.find('button');
 
-    function tab_button(e) {
-        var $target = $(e.target);
-        var tab_id;
-        // var tab_content;
-        var $tab_con = $('.btn_content').find('li');
-        var $tab_img = $('.img_content').find('li');
+//     function tab_button(e) {
+//         var $target = $(e.currentTarget);
+//         var tab_id;
+//         var tab_container = $('.tab_pannel_content').find('div');
+//         var $tab_con = $('.tab_pannel_content').find('[data-id="' + tab_id + '"]');
+        
+//         tab_id = $target.attr('data-id');
+//         $tab_btn.removeClass('active');
+//         $target.addClass('active');
 
-        tab_id = $target.attr('data-id');
-        $tab_btn.removeClass('select__tab');
-        $tab_btn.eq(tab_id).addClass('select__tab');
-
-        if (tab_id == 0) {
-            $tab_con.fadeOut();
-            $tab_con.eq(0).fadeIn();
-            $tab_img.fadeOut();
-            $tab_img.eq(0).fadeIn();
-        } else if (tab_id == 1) {
-            $tab_con.fadeOut();
-            $tab_con.eq(1).fadeIn();
-            $tab_img.fadeOut();
-            $tab_img.eq(1).fadeIn();
-        } else if (tab_id == 2) {
-            $tab_con.fadeOut();
-            $tab_con.eq(2).fadeIn();
-            $tab_img.fadeOut();
-            $tab_img.eq(2).fadeIn();
-        }
-    }
-    $tab_btn.on('click', tab_button);
-}
-());
+//         tab_container.fadeOut();
+//         $tab_con.fadeIn();
+       
+//     }
+//     $tab_btn.on('click', tab_button);
+// }
+// ());
