08a8a1e9db81acc9d3c01b56b7988ea9686313ce
geunyoung <limgeunyoung90@gmail.com>
2018-05-21 17:36:50 +0900

    hamburger ui scss 분리

--- src/js/script/ui-hamburger.js
+++ src/js/script/ui-hamburger.js
@@ -1,17 +1,16 @@
 // hamburger button 클릭시 네비게이션 보임 
 (function () {
-    var $navi_btn = $('[data-ui="hamburger__btn"]');
-    var $navi_list_area = $('[data-ui="navi__area"]');
+    var $hamburger_wrap = $('[data-ui="hamburger_wrap"]');
+    var $hamburger = $hamburger_wrap.find('[data-ui="hamburger__btn"]');
     var $span_before = $('.span__before');
     var $span_after = $('.span__after');
 
-    
-
     function navigation__click(e) {
-        var leftVal;
-        $(this).toggleClass('m_hamburgercloseactive');
+        var target = $(e.currentTarget);
+        target.toggleClass('m_hamburgercloseactive');
+        console.log(target);
 
-        if ($(this).hasClass('m_hamburgercloseactive')) {
+        if (target.hasClass('m_hamburgercloseactive')) {
             TweenMax.to($span_before, 0.2, {
                 rotation: -45,
                 top: 40
@@ -21,9 +20,6 @@
                 bottom: 47
             });
         } else {
-            TweenMax.to($navi_list_area, 0.2, {
-                left: '-100%'
-            });
             TweenMax.to($span_before, 0.2, {
                 rotation: 0,
                 top: 35
@@ -35,5 +31,5 @@
         }
     }
 
-    $navi_btn.on('click', navigation__click);
+    $hamburger.on('click', navigation__click);
 }());
