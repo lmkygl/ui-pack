d2a6bbda3e20f53fa536d78f73182b3bd7b0da29
geunyoung <limgeunyoung90@gmail.com>
2018-05-23 12:27:55 +0900

    modal pop

--- src/js/script/ui-modal.js
+++ src/js/script/ui-modal.js
@@ -1,4 +1,11 @@
 // 버튼 클릭시 modal창 띄우기
 (function(){
-
+    var $page = $('[data-ui="page_wrap"]');
+    var $btn = $page.find('button');
+    
+    function modal_evente(e){
+        var $target = (e.target);
+        
+    }
+    $btn.on('click', modal_event);
 }());
