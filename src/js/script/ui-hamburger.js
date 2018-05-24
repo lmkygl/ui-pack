f126f52fe761a73816c1e89c2071c9f9e33a49e8
geunyoung <limgeunyoung90@gmail.com>
2018-05-24 14:55:47 +0900

    hamburger button

--- src/js/script/ui-hamburger.js
+++ src/js/script/ui-hamburger.js
@@ -10,6 +10,7 @@
         var $line_last = $target.find('span:nth-child(3)');
 
         $(this).toggleClass('is-active');
+
         if ($target.hasClass('is-active')) {
 
             if ($target.hasClass('hamburger_1')) {
@@ -24,16 +25,14 @@
                     y: -13,
                     rotation: -45,
                 });
-            }
-            else if ($target.hasClass('hamburger_2')) {
+            } else if ($target.hasClass('hamburger_2')) {
                 TweenMax.to($line_first, 0.2, {
                     y: 13,
                 });
                 TweenMax.to($line_last, 0.2, {
                     y: -13,
                 });
-            }
-            else if ($target.hasClass('hamburger_3')) {
+            } else if ($target.hasClass('hamburger_3')) {
                 TweenMax.to($line_first, 0.2, {
                     width: 40,
                     x: -10,
@@ -44,8 +43,7 @@
                     x: -10,
                     rotation: 45
                 });
-            }
-            else if ($target.hasClass('hamburger_4')) {
+            } else if ($target.hasClass('hamburger_4')) {
                 TweenMax.to($line_first, 0.2, {
                     width: 40,
                     x: 10,
@@ -56,16 +54,15 @@
                     x: 10,
                     rotation: -45
                 });
-            }
-            else if ($target.hasClass('hamburger_5')) {
-                TweenMax.to(this, 0.2,{
+            } else if ($target.hasClass('hamburger_5')) {
+                TweenMax.to(this, 0.2, {
                     rotation: 90
                 });
                 TweenMax.to($line_first, 0.2, {
                     width: 35,
                     y: 15,
                     rotation: 45,
-                    transformOrigin : 'right'
+                    transformOrigin: 'right'
                 });
                 TweenMax.to($line_sec, 0.2, {
                     opacity: 0
@@ -74,11 +71,10 @@
                     width: 35,
                     y: -15,
                     rotation: -45,
-                    transformOrigin : 'right'
+                    transformOrigin: 'right'
                 });
-            }
-            else if ($target.hasClass('hamburger_6')) {
-                TweenMax.to(this, 0.6,{
+            } else if ($target.hasClass('hamburger_6')) {
+                TweenMax.to(this, 0.6, {
                     rotation: 45
                 });
                 TweenMax.to($line_first, 0.3, {
@@ -91,32 +87,29 @@
                     y: -13,
                     rotation: -90,
                 });
-            }
-            else if ($target.hasClass('hamburger_7')) {
-                TweenMax.to(this, 0.2,{
+            } else if ($target.hasClass('hamburger_7')) {
+                TweenMax.to(this, 0.2, {
                     rotation: 30
                 });
                 TweenMax.to($line_first, 0.3, {
-                   width: 30,
+                    width: 30,
                 });
                 TweenMax.to($line_sec, 0.2, {
                     width: 40
                 });
-            }
-            else if ($target.hasClass('hamburger_8')) {
+            } else if ($target.hasClass('hamburger_8')) {
                 TweenMax.to($line_first, 0.2, {
                     y: 13,
                 });
                 TweenMax.to($line_sec, 0.2, {
-                    opacity:0
+                    opacity: 0
                 });
                 TweenMax.to($line_last, 0.2, {
                     y: -13,
                     rotation: -90,
                 });
-            }
-            else if ($target.hasClass('hamburger_9')) {
-                TweenMax.to(this, 0.2,{
+            } else if ($target.hasClass('hamburger_9')) {
+                TweenMax.to(this, 0.2, {
                     rotation: 45
                 });
                 TweenMax.to($line_first, 0.2, {
@@ -124,24 +117,78 @@
                     width: 35,
                 });
                 TweenMax.to($line_sec, 0.2, {
-                    opacity:0
+                    opacity: 0
                 });
                 TweenMax.to($line_last, 0.2, {
                     y: -13,
                     rotation: 90,
                     width: 35
                 });
+            } else if ($target.hasClass('hamburger_9')) {
+                TweenMax.to(this, 0.2, {
+                    rotation: 45
+                });
+                TweenMax.to($line_first, 0.2, {
+                    y: 13,
+                    width: 35,
+                });
+                TweenMax.to($line_sec, 0.2, {
+                    opacity: 0
+                });
+                TweenMax.to($line_last, 0.2, {
+                    y: -13,
+                    rotation: 90,
+                    width: 35
+                });
+            } else if ($target.hasClass('hamburger_10')) {
+                TweenMax.to(this, 0.2, {
+                    rotation: 90
+                });
+                TweenMax.to($line_first, 0.1, {
+                    width: 30,
+                });
+                TweenMax.to($line_sec, 0.1, {
+                    width: 40,
+                });
+            }
+            // else if ($target.hasClass('hamburger_11')) {
+            //     myFunction() {
+            //         TweenMax.to($line_first, 0.3,{
+            //             scale: 1
+            //         })
+            //     }
+            //     TweenMax.to(this, 0.2, {
+            //         scale: 0,
+            //         onComplete:  myFunction,
+            //     });
+            //     TweenMax.to($line_first, 0.1, {
+
+            //     });
+            //     TweenMax.to($line_sec, 0.1, {
+            //         width: 40,
+            //     });
+            // }
+            else if ($target.hasClass('hamburger_12')) {
+                TweenMax.to($line_first, 0.1, {
+                    opacity: 0,
+                    x: 100
+                });
+                TweenMax.to($line_last, 0.1, {
+                    opacity: 0,
+                    x: -100
+                });
             }
 
         } else {
-            TweenMax.to(this, 0.2,{
+            TweenMax.to(this, 0.2, {
                 rotation: 0
             });
             TweenMax.to($line_first, 0.2, {
                 y: 0,
                 rotation: 0,
                 width: 50,
-                x:0
+                x: 0,
+                opacity: 1,
             });
             TweenMax.to($line_sec, 0.2, {
                 opacity: 1,
@@ -151,7 +198,8 @@
                 y: 0,
                 rotation: 0,
                 width: 50,
-                x:0,
+                x: 0,
+                opacity: 1,
             });
         }
     }
