7fd61afa58a2148a7aee8db47e00db13ee253f32
geunyoung <limgeunyoung90@gmail.com>
2018-07-07 14:47:41 +0900

    ajax test 및 마크업 정리

--- src/js/script/ui-underline.js
+++ src/js/script/ui-underline.js
@@ -1,5 +1,6 @@
 (function () {
     var $a_underline = $('[data-ui="navi_underline"]');
+    console.log($a_underline.length);
 
     function navi_effect(e) {
         var $target = $(e.target);
@@ -16,5 +17,16 @@
         }
     }
 
-    $a_underline.on('mouseenter mouseleave', navi_effect);
+
+    // 동적 init
+    if ($a_underline.length) {
+        $a_underline.on('mouseenter mouseleave', navi_effect);
+    }
+
+    // 비동기 init
+    $(document).ajaxComplete(function(){
+        if ($('[data-ui="navi_underline"]').length) {
+            $('[data-ui="navi_underline"]').on('mouseenter mouseleave', navi_effect);
+        }
+    });
 }());
