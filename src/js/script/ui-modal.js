b3da0fe3088d7b84cb3ada52abf66095793ba430
geunyoung <limgeunyoung90@gmail.com>
2018-09-05 12:12:13 +0900

    modal close btn bug 해결

--- src/js/script/ui-modal.js
+++ src/js/script/ui-modal.js
@@ -3,29 +3,21 @@
     var $page = $('[data-ui="page_wrap"]');
     var $btn = $page.find('button');
     var $modal_wrap = $('[data-ui="modal_wrap"]');
-    var $close_btn = $modal_wrap.find('.trigger');
-
+    
     function modal_event(e) {
         var $target = $(e.currentTarget);
         var $target_parent = $target.parent();
         var modal = $target_parent.siblings();
-        modal.addClass('open');
-        // return false;
+        modal.toggleClass('open');
     }
 
     function modal_close(e) {
-        var $target = $(e.target);
-        $modal_wrap.removeClass('open');
-    }
-
-    function modal_close2(e) {
-        var $target = $(e.target);
-        $(this).removeClass('open');
+        var $target = $(e.currentTarget);
+        $target.removeClass('open');
     }
 
 
     $btn.on('click', modal_event);
-    $close_btn.on('click', modal_close);
     $modal_wrap.on('click', modal_close);
 
 
@@ -34,10 +26,8 @@
         var $page = $('[data-ui="page_wrap"]');
         var $btn = $page.find('button');
         var $modal_wrap = $('[data-ui="modal_wrap"]');
-        var $close_btn = $modal_wrap.find('.trigger');
        
         $btn.on('click', modal_event);
-        $close_btn.on('click', modal_close2);
         $modal_wrap.on('click', modal_close);
 
     });
