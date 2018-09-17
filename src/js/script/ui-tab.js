d9ad41729ed7325ba2b6276a2e3270af3880c67e
geunyoung <limgeunyoung90@gmail.com>
2018-09-17 12:09:06 +0900

    basic tab

--- src/js/script/ui-tab.js
+++ src/js/script/ui-tab.js
@@ -3,25 +3,16 @@
     function tab_button_effect(e) {
         var $target = $(e.target);
         var tab_id;
-        var tab_option;
         var $pannel = $('[data-ui="pannel_area"]');
-
+        var $pannnel_content = $('.tab_pannel');
 
         tab_id = $target.attr('data-id');
         tab_option = $target.attr('data-option');
 
         var $pannel_content = $pannel.children('[data-id="' + tab_id + '"]');
-     //   var $pannel_option_content = $pannel.children('[data-option="' + tab_option + '"]');
 
-       // var $tab_pannel = $('.tab_pannel');
-        //console.log($pannel_option_content);
-
-        $('.tab_pannel').hide();
+        $pannnel_content.hide();
         $pannel_content.show();
-    //    $pannel_option_content.fadeIn();
-        if( tab_id >= 4){
-            $pannel_content.fadeIn();
-        }
 
     }
     $(document).ajaxComplete(function () {
